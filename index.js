afb9f254412cb15e0f25b5d24320070f12dc5bff
Technocation <usmanabid900@gmail.com>
2021-07-10 17:01:21 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+//import App from './App';
+//import Demo from './Demo';
+import Avatar from './Avatar';
+//import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(<Avatar />
+,
+  document.getElementById('root')
+);
+
+//reportWebVitals();
